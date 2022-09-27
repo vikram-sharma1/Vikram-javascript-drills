c1ddd43c71db3efbeaf8a24badd7f5691cc8a903
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-09-27 18:53:04 +0530

    Solution added for callback.js file

--- array-method-impl.js
+++ array-method-impl.js
@@ -9,46 +9,130 @@ const items = [1, 2, 3, 4, 5, 5]; // use this array to test your code.
   You CAN use concat, push, pop, etc. but do not use the exact method that you are replicating
 */
 
-function each(elements, cb) {
-  // Do NOT use forEach to complete this function.
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
-  // This only needs to work with arrays.
-  // You should also pass the index into `cb` as the second argument
-  // based off http://underscorejs.org/#each
+
+function cbEach(num) {
+    console.log(`Indexes are ${num}`)
 }
+
+function each(elements, cb) {
+
+
+    for (let i = 0; i < elements.length; i++) {
+        console.log(`Values are ${elements[i]}`)
+        cb(i)
+    }
+
+
+
+
+}
+
+each(items, cbEach)
+
+// console.log('---------------------------------------------------------------------------------------------');
+
 
 function map(elements, cb) {
-  // Do NOT use .map, to complete this function.
-  // How map works: Map calls a provided callback function once for each element in an array, in order, and functionructs a new array from the res .
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
+
+
+    let newArr = []
+
+    for (let items of elements) {
+        let results = cb(items)
+        newArr.push(results)
+    }
+
+    return (newArr)
+
+
 }
+
+let mapArr = map(items, (element, i, arrRef) => element)
+console.log(mapArr)
+
+
+// console.log('---------------------------------------------------------------------------------------------');
+
+
+const cbReduce = (initialValue, current) => initialValue + current;
 
 function reduce(elements, cb, startingValue) {
-  // Do NOT use .reduce to complete this function.
-  // How reduce works: A reduce function combines all elements into a single value going from left to right.
-  // Elements will be passed one by one into `cb` along with the `startingValue`.
-  // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
-  // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+    let value = startingValue;
+
+    for (let items of elements) {
+        let currentValue = items
+        value = cb(value, currentValue)
+    }
+
+    return value;
 }
+
+const reduceArr = reduce(items, cbReduce, 0)
+
+console.log(reduceArr);
+// console.log('---------------------------------------------------------------------------------------------');
+
+function cbFind(a) {
+    if (!a) {
+      console.log(`undefined`);
+    } else {
+      console.log(a);
+    }
+  }
 
 function find(elements, cb) {
-  // Do NOT use .includes, to complete this function.
-  // Look through each value in `elements` and pass each element to `cb`.
-  // If `cb` returns `true` then return that element.
-  // Return `undefined` if no elements pass the truth test.
+
+    for (let i of elements) {
+        cb(i)
+    }
 }
 
+find(items, cbFind)
+
+
+// console.log('---------------------------------------------------------------------------------------------');
+
 function filter(elements, cb) {
-  // Do NOT use .filter, to complete this function.
-  // Similar to `find` but you will return an array of all elements that passed the truth test
-  // Return an empty array if no elements pass the truth test
+
+
+    let newArr = []
+
+    for (let items of elements) {
+        let results = cb(items)
+        if (results) {
+            newArr.push(items)
+        }
+    }
+
+    return (newArr)
+
+
 }
+
+let filterAns = filter(items, (element, i, arrRef) => element % 2 == 0)
+console.log(filterAns);
+
+
+// console.log('---------------------------------------------------------------------------------------------');
+
 
 const nestedArray = [1, [2], [[3]], [[[4]]]]; // use this to test 'flatten'
 
 function flatten(elements) {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Hint: You can solve this using recursion.
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+
+
+    let finalflattenArray = []
+
+    for (let item of elements) {
+        if (Array.isArray(item)) {
+            finalflattenArray.push(...flatten(item))
+        }
+        else {
+            finalflattenArray.push(item)
+        }
+    }
+
+    return finalflattenArray
 }
+
+console.log(flatten(nestedArray));
