165cf582da572ac57f8415d412948513897910e1
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-09-28 14:29:21 +0530

    New Drill Files added

---
+++ closure.js
@@ -0,0 +1,20 @@
+function counterFactory() {
+    // Return an object that has two methods called `increment` and `decrement`.
+    // `increment` should increment a counter variable in closure scope and return it.
+    // `decrement` should decrement the counter variable and return it.
+  }
+  
+  function limitFunctionCallCount(cb, n) {
+    // Should return a function that invokes `cb`.
+    // The returned function should only allow `cb` to be invoked `n` times.
+    // Returning null is acceptable if cb can't be returned
+  }
+  
+  function cacheFunction(cb) {
+    // Should return a funciton that invokes `cb`.
+    // A cache (object) should be kept in closure scope.
+    // The cache should keep track of all arguments have been used to invoke this function.
+    // If the returned function is invoked with arguments that it has already seen
+    // then it should return the cached result and not invoke `cb` again.
+    // `cb` should only ever be invoked once for a given set of arguments.
+  }
