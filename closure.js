08abe93b2e6dfb59724c021e8898074032bc10c9
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-09-28 16:58:08 +0530

    Solved sol added file

--- closure.js
+++ closure.js
@@ -2,6 +2,16 @@ function counterFactory() {
     // Return an object that has two methods called `increment` and `decrement`.
     // `increment` should increment a counter variable in closure scope and return it.
     // `decrement` should decrement the counter variable and return it.
+
+      let count = 0
+
+      return {
+        increment : () => count++ ,
+        decrement : () => count--
+      }
+
+
+
   }
   
   function limitFunctionCallCount(cb, n) {
--- object-method.js
+++ object-method.js
@@ -7,23 +7,39 @@ function keys(obj) {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+
+  return Object.keys(obj)
 }
 
 function values(obj) {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+
+  return Object.values(obj)
+
 }
 
 function mapObject(obj, cb) {
   // Like map for arrays, but for objects. Transform the value of each property in turn by passing it to the callback function.
   // http://underscorejs.org/#mapObject
+
+  Object.keys(obj).forEach(key => (cb(obj[key])));
+  return obj;
+
+
 }
 
 function pairs(obj) {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+
+  return Object.keys(obj).map((key) => [key, obj[key]])
+
+
+
 }
+
 
 /* STRETCH PROBLEMS */
 
@@ -31,7 +47,18 @@ function invert(obj) {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  var result = {};
+  Object.keys(obj).map((key) => {
+    result[obj[key]] = key
+  });
+  return result;
+  
+  
+  // console.log(result[obj[key]] = key, [obj[key]])
 }
+
+// console.log(invert(testObject))
+
 
 function defaults(obj, defaultProps) {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
