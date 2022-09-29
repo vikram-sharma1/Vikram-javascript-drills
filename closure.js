fe318578ecb80ca509ad57a09bcb30cc1e5b776e
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-09-29 16:57:15 +0530

    Added Sol to Closure

--- closure.js
+++ closure.js
@@ -18,6 +18,15 @@ function counterFactory() {
     // Should return a function that invokes `cb`.
     // The returned function should only allow `cb` to be invoked `n` times.
     // Returning null is acceptable if cb can't be returned
+
+    let callCount = 0;
+      return (...args) => {
+        if (callCount === n){
+          return null
+        } 
+        callCount++;
+        return cb(...args);
+      };
   }
   
   function cacheFunction(cb) {
