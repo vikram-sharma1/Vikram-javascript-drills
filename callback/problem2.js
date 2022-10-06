8aa409df13912dffc079670d744aa10417b1c637
Vikram Shrama <thevikramdeveloper@gmail.com>
2022-10-06 18:26:46 +0530

    solved P2 with formatting

--- callback/problem2.js
+++ callback/problem2.js
@@ -10,135 +10,129 @@
         5. Read the contents of filenames.txt and delete all the new files that are mentioned in that list simultaneously.
 */
 
-
-
 const fs = require('fs')
 
 let num = Math.round(Math.random() * 50)
 
-fs.readFile('./lipsum.txt', "utf-8", (err, data) => {
-    if(err){
-        console.log(err)
-    }
-    else{
-        // console.log(data)
-        
-            fs.writeFile(`./writeContent/upperCase${num}.txt`, data.toUpperCase(), (err) => {
-                if(err){
-                    console.log(err)
-                    return
-                }
-                else{
-                    console.log(`sucessfully converted the file`);
+fs.readFile('./lipsum.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    // console.log(data)
 
-                        let datainSen = data.toLowerCase().split('.').join('\n')
+    fs.writeFile(
+      `./writeContent/upperCase${num}.txt`,
+      data.toUpperCase(),
+      (err) => {
+        if (err) {
+          console.log(err)
+          return
+        } else {
+          console.log(`sucessfully converted the file`)
 
-                            // console.log(datainSen)
-                            fs.writeFile(`./writeContent/upperCase${num}.txt`,datainSen, "utf-8", (err, data) => {
-                                if(err){
+          let datainSen = data.toLowerCase().split('.').join('\n')
+
+          // console.log(datainSen)
+          fs.writeFile(
+            `./writeContent/upperCase${num}.txt`,
+            datainSen,
+            'utf-8',
+            (err, data) => {
+              if (err) {
+                console.log(err)
+                return
+              } else {
+                console.log('data changed')
+
+                fs.appendFile(
+                  `./writeContent/breakSenLowerCase${num}.txt`,
+                  datainSen,
+                  (err) => {
+                    if (err) {
+                      console.log(err)
+                    } else {
+                      console.log('new sentence file created')
+
+                      datainSen = datainSen.split('\n').sort().join('\n')
+                      // console.log(datainSen)
+
+                      fs.appendFile(
+                        `./writeContent/dataSortedLowerCase${num}.txt`,
+                        datainSen,
+                        'utf8',
+                        (err) => {
+                          if (err) {
+                            console.log(err)
+                          } else {
+                            fs.readdir('./writeContent', (err, files) => {
+                              if (err) {
+                                console.log(err)
+                                return
+                              } else {
+                                // console.log(files)
+
+                                fs.unlink(`./filenames.txt`, (err) => {
+                                  if (err) {
                                     console.log(err)
                                     return
-                                }
-                                else{
-                                    console.log("data changed") 
+                                  }
+                                })
 
-                                    fs.appendFile(`./writeContent/breakSenLowerCase${num}.txt`, datainSen, (err) => {
-                                        if(err){
-                                            console.log(err)
-                                        }
-                                        else{
-                                            console.log('new sentence file created')
+                                files.forEach((singleName) => {
+                                  fs.appendFile(
+                                    './filenames.txt',
+                                    `${singleName} `,
+                                    (err) => {
+                                      if (err) {
+                                        console.log(err)
+                                        return
+                                      } else {
+                                      }
+                                    },
+                                  )
+                                })
+                              }
 
-                                                
-
-                                        datainSen = datainSen.split('\n').sort().join('\n')
-                                        // console.log(datainSen)
-
-                                            
-                                                fs.appendFile(`./writeContent/dataSortedLowerCase${num}.txt`, datainSen, "utf8", (err)=> {
-                                                    if(err){
-                                                        console.log(err);
-                                                    }else{
-
-                                                        fs.readdir('./writeContent', (err, files) => {
-                                                            if(err){
-                                                                console.log(err)
-                                                                return
-                                                            }
-                                                            else{
-                                                                // console.log(files)
-                                
-                                                                fs.unlink(`./filenames.txt`, (err) => {
-                                                                    if (err) {
-                                                                    console.log(err)
-                                                                    return;
-                                                                    }
-                                                                });     
-                                
-                                
-                                                                files.forEach((singleName) => {
-                                                                    fs.appendFile('./filenames.txt', `${singleName} ` , (err) => {
-                                                                        if(err){
-                                                                            console.log(err)
-                                                                            return
-                                                                        }
-                                                                        else{
-
-                                                                            
-                                                                        }
-                                                                    })
-                                
-                                                                })
-                                                                
-                                                            }
-
-                                                            fs.readFile('./filenames.txt', "utf-8", (err, data) => {
-                                                                if(err){
-                                                                    console.log(err)
-                                                                    return
-                                                                }
-                                                                else{
-                                                                    let deleteList = data.split(' ').slice(0,-1)
-                                                                    // console.log(deleteList)
-                                                                    deleteList.forEach((singleFile) => {
-                                                                        fs.unlink(`./writeContent/${singleFile}`, (err) => {
-                                                                          if (err) {
-                                                                            console.log('line',err  )
-                                                                            return;
-                                                                          }
-                                                                          else{
-                                                                            console.log('All clear')
-                                                                          }
-                                                                        });
-                                                                      });
-                                                                }
-                                                            })
-                                
-                                                        })
-
-                                                       
-
-
-
-
-                                                    }
-                                                })
-
-
-
-
-                                        }
+                              fs.readFile(
+                                './filenames.txt',
+                                'utf-8',
+                                (err, data) => {
+                                  if (err) {
+                                    console.log(err)
+                                    return
+                                  } else {
+                                    let deleteList = data
+                                      .split(' ')
+                                      .slice(0, -1)
+                                    // console.log(deleteList)
+                                    deleteList.forEach((singleFile) => {
+                                      fs.unlink(
+                                        `./writeContent/${singleFile}`,
+                                        (err) => {
+                                          if (err) {
+                                            console.log('line', err)
+                                            return
+                                          } else {
+                                            console.log('All clear')
+                                          }
+                                        },
+                                      )
                                     })
-                                    
-                                }
+                                  }
+                                },
+                              )
                             })
-
-
-                }
-            })
-
-
-    }
+                          }
+                        },
+                      )
+                    }
+                  },
+                )
+              }
+            },
+          )
+        }
+      },
+    )
+  }
 })
-
-
