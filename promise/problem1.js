bdc12694ecccf58a18f3392551e2dd8c92187305
Vikram Shrama <thevikramdeveloper@gmail.com>
2022-10-08 09:45:56 +0530

    Resolved Problem 1 of Promise

--- promise/problem1.js
+++ promise/problem1.js
@@ -2,40 +2,45 @@ const fs = require('fs').promises
 
 const randomNum = Math.round(Math.random() * 50)
 
-const makedir = fs.mkdir(`./p1Dir${randomNum}`)
+const makedir = fs.mkdir(`./p1Dir${randomNum}`,)
+
+let content = {
+  data: 'Testing1',
+  data2: 'Testing2',
+}
 
 makedir
   .then(() => {
     console.log('Directory Created')
 
     const numToCreateFile = Math.round(Math.random() * 5 + 2)
-    let content = {
-      data: 'Testing1',
-      data2: 'Testing2',
-    }
-
+    
+    let arr = []
     for (let i = 0; i < numToCreateFile; i++) {
       const numForFiles = Math.round(Math.random() * 50)
 
-      fs.writeFile(
+      arr.push(fs.writeFile(
         `./p1Dir${randomNum}/file${numForFiles}.json`,
         JSON.stringify(content),
         'utf-8',
-      )
+      ))
     }
 
     console.log(`Number of Files Created ${numToCreateFile}`)
+    return Promise.all(arr)
   })
-  .then((data) => {
+  .then(() => {
     let filesName = fs.readdir(`./p1Dir${randomNum}`)
     return filesName
   })
   .then((data) => {
-    console.log(data)
+    let del = []
     data.forEach((singleFileName) => {
-      const deleteFileRes = fs.unlink(`./p1Dir${randomNum}/${singleFileName}`)
-      console.log('File Deleted')
-      return deleteFileRes
+      del.push(fs.unlink(`./p1Dir${randomNum}/${singleFileName}`))
     })
+
+    return Promise.all(del)
+  }).then(()=>{
+    console.log('All files Deleted')
   })
   .catch((err) => console.log(err))
